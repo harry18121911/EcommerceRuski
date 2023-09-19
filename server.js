c4cf96ec0e71f43eb12118b0731559e748ec1fea
harry18121911 <harrytheripper1812@hotmail.com.ar>
2023-09-19 08:27:55 -0300

    1 hora terminado, POST exitoso.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-
+import authRoutes from './routes/authRoute.js';
 //configure dotenv
 dotenv.config();
 
@@ -15,6 +15,10 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+
+//routes
+
+app.use('/api/v1/auth', authRoutes);
 
 //rest api
 app.get('/', (req, res) =>{
