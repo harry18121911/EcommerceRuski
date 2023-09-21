fb9a771fc4135ddfecab7b01da0e1659ece69d7a
harry18121911 <harrytheripper1812@hotmail.com.ar>
2023-09-21 09:36:04 -0300

    Antes de empezar Registro.

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import {Routes,Route} from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import Policy from './pages/Policy.js';
+import PagenotFound from './pages/PagenotFound.js'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/policy' element={<Policy/>} />
+      <Route path='/*' element={<PagenotFound/>} />
+    </Routes>
+   
+  );
+}
+
+export default App;
---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import {Helmet} from "react-helmet";
+const Layout = ({children, title, description, keywords, author})=>{
+    return(
+        <div>
+            <Helmet>
+            <meta charSet='utf-8'/>
+            <meta name='decription' content={description} />
+            <meta name='keywords' content={keywords} />
+            <meta name='author' content={author} />
+            <title>{title}</title>
+            </Helmet>
+            <Header/>
+            <main style={{minHeight:'70vh'}}>
+            {children}
+            </main>
+            <Footer/>
+        </div>
+    )
+};
+
+Layout.defaultProps={
+    title:"Ecommerce app",
+    description: "mern stack project",
+    keywords: "mern, react, node, mongodb",
+    author: "harry18121911",
+}
+;
+export default Layout;
