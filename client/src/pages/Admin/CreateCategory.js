0c138d9dd2943d21863efd382ae3feb8d2cc8005
harry18121911 <harrytheripper1812@hotmail.com.ar>
2023-10-18 11:57:00 -0300

    Catefory CRUD finalizado

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -4,10 +4,15 @@ import AdminMenu from '../../components/Layout/AdminMenu'
 import toast from "react-hot-toast";
 import axios from 'axios';
 import CategoryForm from '../../components/Form/CategoryForm';
+import {Modal} from "antd";
 
 const CreateCategory = () => {
     const [categories, setCategories] = useState([]);
     const [name, setName] = useState("");
+    const [visible, setVisible] = useState(false);
+    const [selected, setSelected] = useState(null);
+    const [updatedName, setUpdatedName] = useState("");
+
     //handle form
     const handleSubmit = async(e) =>{
         e.preventDefault()
@@ -42,9 +47,41 @@ const CreateCategory = () => {
         getAllCategory();
     }, []);
   
-  
-  
-  
+    //update category
+    const handleUpdate = async (e) =>{
+        e.preventDefault();
+        try {
+            const {data} = await axios.put(`${process.env.REACT_APP_API}/api/v1/category/update-category/${selected._id}`, {name:updatedName})
+            if(data.success){
+                toast.success(`${updatedName} is updated`);
+                setSelected(null);
+                setUpdatedName("");
+                setVisible(false);
+                getAllCategory();
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error('Something went wrong');
+        }       
+    };
+
+    //delete category
+    const handleDelete = async (pId) =>{
+        
+        try {
+            const {data} = await axios.delete(`${process.env.REACT_APP_API}/api/v1/category/delete-category/${pId}`, {name:updatedName})
+            if(data.success){
+                toast.success(`Category is deleted`);               
+                getAllCategory();
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error('Something went wrong');
+        }       
+    };
+
     return (
     <Layout title={"Dashboard - Create Category"}>
         <div className='container-fluid m-3 p-3'>
@@ -74,13 +111,23 @@ const CreateCategory = () => {
                         <>
                             <tr>
                                 <td key={c._id}>{c.name}</td>
-                                <td><button className='btn btn-primary'>Edit</button></td>
+                                <td>
+                                    <button className='btn btn-primary ms-2' onClick={() => {setVisible(true) ; setUpdatedName(c.name); setSelected(c)}}>Edit</button>
+                                    <button className='btn btn-danger ms-2' onClick={() =>{handleDelete(c._id)}}>Delete</button>
+                                </td>                               
                             </tr>
                         </>
                     ))}                                                  
                     </tbody>
                     </table>
                     </div>
+                    <Modal 
+                        onCancel={()=>setVisible(false)} 
+                        footer={null} 
+                        visible={visible}
+                    >
+                        <CategoryForm value={updatedName} setValue={setUpdatedName} handleSubmit={handleUpdate}/>
+                    </Modal>
                 </div>
             </div>
         </div>
